ae94cc7ec4416494f5410fccb4e3ac69245ca351
Facu <facuassain@gmail.com>
2020-11-09 10:41:35 -0300

    * Se modularizó la apertura y el cierre de los archivos ifstream * Se borraron los cout de los registros, ya que ya se verificó que eran correctamente leídos

--- tpV2.cpp
+++ tpV2.cpp
@@ -25,7 +25,8 @@ struct Node
     Node* next;
 };
 
-
+void Abrir(ifstream&,ifstream&);
+void Cerrar(ifstream&,ifstream&);
 void procParteDiario(ifstream&,Node*);
 void leerParteDiario(ifstream&,tsParDia&);
 void procPaises(ifstream&,Node*);
@@ -33,21 +34,29 @@ void leerPais(ifstream&,tsPais&);
 
 int main(int argc, char const *argv[])
 {
-    ifstream arParteDiarioBIN("ParteDiario.bin", ios::in|ios::binary);
-    ifstream arPaisesBIN("Paises.bin", ios::in|ios::binary);
+    ifstream arParteDiarioBIN, arPaisesBIN;
 
-    
+    Abrir(arPaisesBIN,arParteDiarioBIN);
     Node* head = NULL;
     Node* headPais = NULL;
 
     procPaises(arPaisesBIN,headPais);
     procParteDiario(arParteDiarioBIN,head);
 
-    arParteDiarioBIN.close();
-    arPaisesBIN.close();
+    Cerrar(arPaisesBIN,arParteDiarioBIN);
+    
     return 0;
 }
 
+void Abrir(ifstream &arPaises, ifstream &arParDia){
+    arPaises.open("Paises.bin", ios::in|ios::binary);
+    arParDia.open("ParteDiario.bin", ios::in|ios::binary);
+}
+
+void Cerrar(ifstream &arPaises, ifstream &arParDia){
+    arPaises.close();
+    arParDia.close();
+}
 void leerParteDiario(ifstream &arParDia, tsParDia &registro){
     
     arParDia.read((char*)&registro, sizeof(registro));
@@ -58,39 +67,32 @@ void leerParteDiario(ifstream &arParDia, tsParDia &registro){
 }
 
 void procParteDiario(ifstream &arParDia, Node* head_ref){
+
     tsParDia registro;
+
     while(!arParDia.eof()){
         leerParteDiario(arParDia,registro);
 
         if(strcmp(registro.nombrePais,"") == 0){
             break;
         }
-        
-        cout<<registro.nombrePais<<endl;
-        cout<<registro.mes<<endl;
-        cout<<registro.dia<<endl;
-        cout<<registro.cantidades[0]<<endl;
-        cout<<registro.cantidades[1]<<endl;
-        cout<<registro.cantidades[2]<<endl;
-        cout<<registro.cantidades[3]<<endl;
-        cout<<arParDia.tellg()<<endl<<endl;
+
+        /* Logica de guardado del registro */
     }
 }
 
 void procPaises(ifstream &arPaises, Node* head_ref){
     
     tsPais regPais;
+
     while(!arPaises.eof()){
         leerPais(arPaises,regPais);
 
         if(strcmp(regPais.nombrePais, "") == 0){
             break;
         }
-
-        cout<<regPais.nombrePais<<endl;
-        cout<<regPais.continente<<endl;
-        cout<<regPais.cantHabitantes<<endl;
-        cout<<arPaises.tellg()<<endl<<endl;
+        
+        /* Logica de guardado del registro */
     }
 }
 
