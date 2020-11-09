bde908c37f99350424d9aa1cd4bcc8d0ed55b708
Facu <facuassain@gmail.com>
2020-11-09 10:30:47 -0300

    Al parecer en el compilador gcc, sucede lo mismo que en el mingw, se entra al while a pesar de tener el flag = -1, asi que se agregó la misma validación que en la branch 'devccompiler', salir del while en caso de que exista ese flag

--- tpV2.cpp
+++ tpV2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string.h>
 
 using namespace std;
 
@@ -42,13 +43,17 @@ int main(int argc, char const *argv[])
     procPaises(arPaisesBIN,headPais);
     procParteDiario(arParteDiarioBIN,head);
 
-
+    arParteDiarioBIN.close();
+    arPaisesBIN.close();
     return 0;
 }
 
 void leerParteDiario(ifstream &arParDia, tsParDia &registro){
     
     arParDia.read((char*)&registro, sizeof(registro));
+    if(!arParDia.good()){
+        strcpy(registro.nombrePais,"");
+    }
     
 }
 
@@ -56,6 +61,19 @@ void procParteDiario(ifstream &arParDia, Node* head_ref){
     tsParDia registro;
     while(!arParDia.eof()){
         leerParteDiario(arParDia,registro);
+
+        if(strcmp(registro.nombrePais,"") == 0){
+            break;
+        }
+        
+        cout<<registro.nombrePais<<endl;
+        cout<<registro.mes<<endl;
+        cout<<registro.dia<<endl;
+        cout<<registro.cantidades[0]<<endl;
+        cout<<registro.cantidades[1]<<endl;
+        cout<<registro.cantidades[2]<<endl;
+        cout<<registro.cantidades[3]<<endl;
+        cout<<arParDia.tellg()<<endl<<endl;
     }
 }
 
@@ -64,13 +82,21 @@ void procPaises(ifstream &arPaises, Node* head_ref){
     tsPais regPais;
     while(!arPaises.eof()){
         leerPais(arPaises,regPais);
+
+        if(strcmp(regPais.nombrePais, "") == 0){
+            break;
+        }
+
+        cout<<regPais.nombrePais<<endl;
+        cout<<regPais.continente<<endl;
+        cout<<regPais.cantHabitantes<<endl;
+        cout<<arPaises.tellg()<<endl<<endl;
     }
 }
 
 void leerPais(ifstream &arPaises, tsPais &regPais){
     arPaises.read((char*)&regPais, sizeof(regPais));
-    cout<<regPais.nombrePais<<endl;
-    cout<<regPais.continente<<endl;
-    cout<<regPais.cantHabitantes<<endl<<endl;
-    
+    if(!arPaises.good()){
+        strcpy(regPais.nombrePais,"");
+    }
 }
