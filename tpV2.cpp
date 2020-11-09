7ccea4cdfa181ff4f1b3d0d5e4930da20eb2728f
facundo <facuassain@gmail.com>
2020-11-09 10:17:14 -0300

    Al parecer el compilador del devc++ (mingw) entra al condicional del while, aunque tenga el flag = -1, por lo que se agrego un condicional para evaluar si se está en esa posición del puntero y se sale del while mediante un break

--- tpV2.cpp
+++ tpV2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string.h>
 
 using namespace std;
 
@@ -40,6 +41,7 @@ int main(int argc, char const *argv[])
     Node* headPais = NULL;
 
     procPaises(arPaisesBIN,headPais);
+    
     procParteDiario(arParteDiarioBIN,head);
 
 
@@ -50,15 +52,9 @@ void leerParteDiario(ifstream &arParDia, tsParDia &registro){
     
     arParDia.read((char*)&registro, sizeof(registro));
     if(!arParDia.good()){
+    	strcpy(registro.nombrePais,"");
     	return;
 	}
-    cout<<registro.nombrePais<<endl;
-    cout<<registro.mes<<endl;
-    cout<<registro.dia<<endl;
-    cout<<registro.cantidades[0]<<endl;
-    cout<<registro.cantidades[1]<<endl;
-    cout<<registro.cantidades[2]<<endl;
-    cout<<registro.cantidades[3]<<endl<<endl;
     
 }
 
@@ -67,6 +63,16 @@ void procParteDiario(ifstream &arParDia, Node* head_ref){
     cout<<"================= REGISTROS PARTE DIARIO ================="<<endl;
     while(!arParDia.eof()){
         leerParteDiario(arParDia,registro);
+        if(strcmp(registro.nombrePais, "") == 0){
+        	break;
+		}
+        cout<<registro.nombrePais<<endl;
+    	cout<<registro.mes<<endl;
+    	cout<<registro.dia<<endl;
+    	cout<<registro.cantidades[0]<<endl;
+    	cout<<registro.cantidades[1]<<endl;
+    	cout<<registro.cantidades[2]<<endl;
+    	cout<<registro.cantidades[3]<<endl<<endl;
     }
 }
 
@@ -76,16 +82,20 @@ void procPaises(ifstream &arPaises, Node* head_ref){
     cout<<"================= REGISTROS PAISES ================="<<endl;
     while(!arPaises.eof()){
         leerPais(arPaises,regPais);
+        if(strcmp(regPais.nombrePais, "") == 0){
+        	break;
+		}
+        cout<<regPais.nombrePais<<endl;
+    	cout<<regPais.continente<<endl;
+    	cout<<regPais.cantHabitantes<<endl<<endl;
     }
 }
 
 void leerPais(ifstream &arPaises, tsPais &regPais){
     arPaises.read((char*)&regPais, sizeof(regPais));
     if(!arPaises.good()){
+    	strcpy(regPais.nombrePais,"");
     	return;
 	}
-    cout<<regPais.nombrePais<<endl;
-    cout<<regPais.continente<<endl;
-    cout<<regPais.cantHabitantes<<endl<<endl;
     
 }
